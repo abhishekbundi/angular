47c29fde3672d2ddef69588827c8cc5de58b79d2
abhishekbundi <sainibundi123@gmail.com>
2022-06-03 09:05:30 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import {AboutComponent} from './components/about/about.component';
+import {ContactComponent} from './components/contact/contact.component';
+import {GalleryComponent} from './components/gallery/gallery.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+{path:'',component:HomeComponent},
+{ path: 'contact', component: ContactComponent,pathMatch: 'full' },
+{ path: 'gallery', component: GalleryComponent,pathMatch: 'full' },
+{ path: 'about', component: AboutComponent,pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent,
+    GalleryComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
